198bcee0bb2f9b748c927e6b0045805523197839
Adeeb Bayat <adeebbayat@yahoo.com>
2023-10-06 08:05:40 -0700

    Initial Commit

--- react/luke-api-walker/src/App.js
+++ react/luke-api-walker/src/App.js
@@ -4,10 +4,13 @@ import axios from 'axios';
 import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
+
+
 function App() {
   const [Id,setId] = useState("");
   const [characterData, setCharacterData] = useState({})
   const [planetData,setPlanetData] = useState({})
+  const [PP,setPP] = useState("people")
   
   const navigate = useNavigate();
   
@@ -15,6 +18,7 @@ function App() {
   const submitHandler = (e) =>{
     e.preventDefault();
     const peoplePlanet = document.getElementById("personPlace").value
+    setPP(peoplePlanet)
     navigate(`/${peoplePlanet}/${Id}`)
     axios.get(`https://swapi.dev/api/${peoplePlanet}/${Id}`).then(response=>{
       
@@ -28,7 +32,7 @@ function App() {
       }
       
       
-  })}
+  }).catch((err) => setPP(err))}
     
 
   
@@ -38,7 +42,7 @@ function App() {
       <div className = 'flex'>
           <h3>Search for:</h3>
           <select name="personPlace" id="personPlace">
-            <option value="people">People</option>
+            <option selected="selected" value="people">People</option>
             <option value="planets">Planets</option>
           </select>
           <h3>ID:</h3>
@@ -47,27 +51,32 @@ function App() {
       </div>
       </form>
       <div>
-      {document.getElementById("personPlace").value === "people" ? (
-      characterData && (
-          <div className='left'>
-            <h2>{characterData.name}</h2>
-            <p>Height: {characterData.height}</p>
-            <p>Mass: {characterData.mass}</p>
-            <p>Hair Color: {characterData.hair_color}</p>
-            <p>Skin Color: {characterData.skin_color}</p>
-          </div>
-          )
-      ):(
-        planetData && (
-          <div className = 'left'>
-            <h2>{planetData.name}</h2>
-            <p>Climate: {planetData.climate}</p>
-            <p>Terrain: {planetData.terrain}</p>
-            <p>Surface Water: {planetData.surface_water}</p>
-            <p>Population: {planetData.population}</p>
-          </div>
-        )
-      )}
+      {PP === "people" ? (
+          
+            <div className='left'>
+              <h2>{characterData.name}</h2>
+              <p>Height: {characterData.height}</p>
+              <p>Mass: {characterData.mass}</p>
+              <p>Hair Color: {characterData.hair_color}</p>
+              <p>Skin Color: {characterData.skin_color}</p>
+            </div>
+          
+        ) : PP === "planets" ? (
+          
+            <div className='left'>
+              <h2>{planetData.name}</h2>
+              <p>Climate: {planetData.climate}</p>
+              <p>Terrain: {planetData.terrain}</p>
+              <p>Surface Water: {planetData.surface_water}</p>
+              <p>Population: {planetData.population}</p>
+            </div>
+          
+        ) : (
+        <div>
+          <h1>These aren't the droids you're looking for</h1>
+          <img src="https://th.bing.com/th/id/OIP.5mIw_6GvgAPRT46FeCs1mQAAAA?pid=ImgDet&rs=1"/>
+        </div>
+        )}
       </div>
     </div>
   );
